a360ed94c6271b40364fc04f00f642ceae3ac906
Yashaswini Jain <122783078+yoshjayn@users.noreply.github.com>
2025-02-14 22:08:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.querySelector("#form1").addEventListener("submit", function(event) {
         event.preventDefault(); // Prevent form submission
-        let first = document.querySelector("#fname").value.trim();
-        let last = document.querySelector("#lname").value.trim();
+        let first = document.querySelector("input[name='fname']").value.trim();
+        let last = document.querySelector("input[name='lname']").value.trim();
         
         if (first !== "" && last !== "") {
             alert(`${first} ${last}`);
