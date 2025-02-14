627632c51bfc03dceef446735112a7795fe56b53
Yashaswini Jain <122783078+yoshjayn@users.noreply.github.com>
2025-02-14 22:02:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function getFormvalue() {
-    //Write your code here
-
-}
+document.querySelector("#form1").addEventListener("submit", function(event) {
+        event.preventDefault(); // Prevent form submission
+        let first = document.querySelector("#fname").value.trim();
+        let last = document.querySelector("#lname").value.trim();
+        
+        if (first !== "" && last !== "") {
+            alert(`${first} ${last}`);
+        }
+        else (alert("enter valid inputs!"))
+    });
